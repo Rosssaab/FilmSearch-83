11cd62b442b6bb89d15711c8a8bfdca08bd9eefa
Rosssaab <rosssaab@live.com>
2024-10-14 10:33:39 +0100

    Finalize all features and fix Read more functionality

--- app.py
+++ app.py
@@ -165,4 +165,6 @@ def themed_index(theme):
     return index()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8083)
+    from waitress import serve
+    print("Starting server on http://localhost:8083")
+    serve(app, host='0.0.0.0', port=8083)
